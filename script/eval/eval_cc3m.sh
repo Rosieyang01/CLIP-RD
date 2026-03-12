c1462128478489fd9fa9b16c495b8cbb19d79af7
Rosieyang01 <tnqlsrhksk00@gmail.com>
2026-03-12 15:43:47 +0900

    add eval
    eval

---
+++ script/eval/eval_cc3m.sh
@@ -0,0 +1,24 @@
+cd src
+torchrun --nproc_per_node 2 -m \
+    --master_addr=127.0.0.3 --master_port=29533 \
+    training.main \
+    --save-frequency 1 \
+    --zeroshot-frequency 1 \
+    --report-to tensorboard \
+    --train-data="path/to/cc3m_train.csv,path/to/cc12m.csv"  \
+    --val-data="path/to/cocoir/"  \
+    --csv-img-key filepath \
+    --csv-caption-key title \
+    --data-root path/to/cc3m/images/,path/to/cc12m/images/ \
+    --val-data-root path/to/cc3m_val \
+    --warmup 10000 \
+    --batch-size=512 \
+    --lr=1e-3 \
+    --wd=0.1 \
+    --epochs 32 \
+    --workers=16 \
+    --model ViT-T-16 \
+    --resume path/to/model.pt \
+    --logs path/to/logs/  \
+    --eval \
+    --tag eval_cc3m_val
