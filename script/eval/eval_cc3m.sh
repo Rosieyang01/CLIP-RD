0cb62096df6351f92decbeb6e1ebbcbcce93829c
Rosieyang01 <144993283+Rosieyang01@users.noreply.github.com>
2026-03-13 01:31:53 +0900

    Update eval_cc3m.sh

--- script/eval/eval_cc3m.sh
+++ script/eval/eval_cc3m.sh
@@ -7,11 +7,11 @@ torchrun --nproc_per_node 1 -m \
     --zeroshot-frequency 1 \
     --report-to tensorboard \
     --train-data="path/to/cc3m_train.csv,path/to/cc12m.csv"  \
-    --val-data="path/to/cocoir/"  \
-    --csv-img-key filepath \
-    --csv-caption-key title \
+    --val-data="path/to/cc3m/val"  \
     --data-root path/to/cc3m/images/,path/to/cc12m/images/ \
     --val-data-root path/to/cc3m_val \
+    --csv-img-key filepath \
+    --csv-caption-key title \
     --warmup 10000 \
     --batch-size=1024 \
     --lr=1e-3 \
@@ -19,7 +19,6 @@ torchrun --nproc_per_node 1 -m \
     --epochs 32 \
     --workers=16 \
     --model ViT-T-16 \
-    --t-model ViT-B-16 \
     --resume path/to/model.pt \
     --logs path/to/logs/  \
     --tag eval_cc3m_val
